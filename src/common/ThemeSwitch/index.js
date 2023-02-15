398cbeb2d9ba14daa102440f13427ca3ac4e715e
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-02-15 15:41:45 +0100

    created themeSlice

--- src/common/ThemeSwitch/index.js
+++ src/common/ThemeSwitch/index.js
@@ -1,11 +1,14 @@
+import { useSelector, useDispatch } from "react-redux";
 import { Wrapper, Button, Text, Container, IconWrapper, Icon } from "./styled";
+import { selectIsDarkTheme, toggleTheme } from "./themeSlice";
 
 export const ThemeSwitch = () => {
-    const isDarkTheme = false;
+    const isDarkTheme = useSelector(selectIsDarkTheme);
+    const dispatch = useDispatch();
 
     return (
         <Wrapper>
-            <Button>
+            <Button onClick={() => dispatch(toggleTheme())}>
                 <Text>Dark mode {isDarkTheme ? "on" : "off"}</Text>
                 <Container>
                     <IconWrapper moveToRight={isDarkTheme}>
