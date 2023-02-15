200fd97e54751e71be4e70be0c8e2390f445bf39
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-02-15 16:37:43 +0100

    getting theme settings form state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { useSelector } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
+import { selectIsDarkTheme } from './common/ThemeSwitch/themeSlice';
 import { PersonalHomepage } from './features/homepage';
 import { themeDark, themeLight } from './theme';
 
 
 function App() {
-const isDarkTheme = false;
+  const isDarkTheme = useSelector(selectIsDarkTheme);
   return (
     <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
       <PersonalHomepage />
