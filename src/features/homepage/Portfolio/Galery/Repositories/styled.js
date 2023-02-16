297eaf02610bb101e654cc85b5047b81e9fc216c
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-02-16 19:42:29 +0100

    added missing transition on repositories tiles

--- src/features/homepage/Portfolio/Galery/Repositories/styled.js
+++ src/features/homepage/Portfolio/Galery/Repositories/styled.js
@@ -20,6 +20,7 @@ export const Container = styled.li`
     border: 6px solid rgba(209, 213, 218, 0.3);
     box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
     border-radius: 4px;
+    transition: border 0.3s;
 
     &:hover {
         border: 6px solid rgba(3, 102, 214, 0.2);
